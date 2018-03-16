2bbb55ed42a7c4154961614f20afa23a125de561
r12a <ishida@w3.org>
2018-03-16 10:37:27 +0000

    jazh tests: rewrite to incorporate Koji's suggestions and some other changes

--- results/line-breaks-jazh-data/manifest.js
+++ results/line-breaks-jazh-data/manifest.js
@@ -1,18 +1,18 @@
 var tests = {
 	'ja/css-text-line-break-ja-cj-strict.html': [
-	'CSS3 Text, linebreaks: Conditional Japanese Starter (CJ) (strict,ja)',
+	'line-break:strict, Conditional Japanese Starter (CJ) (ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
 	'With line-break:strict, a browser will NOT allow a conditional Japanese starter at the beginning of a line.',
 	],
 	'ja/css-text-line-break-ja-cj-normal.html': [
 	'Line-break:normal, Conditional Japanese Starter (CJ) (ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] The browser WILL allow a conditional Japanese starter at the beginning of a line.',
+	'[Exploratory] The browser may allow a conditional Japanese starter at the beginning of a line.',
 	],
 	'ja/css-text-line-break-ja-cj-loose.html': [
 	'Line-break:loose, Conditional Japanese Starter (CJ) (ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] The browser WILL allow a conditional Japanese starter at the beginning of a line.',
+	'[Exploratory] The browser may allow a conditional Japanese starter at the beginning of a line.',
 	],
 	'ja/css-text-line-break-ja-hyphens-strict.html': [
 	'CSS3 Text, linebreaks: hyphens (strict,ja)',
@@ -40,9 +40,9 @@ var tests = {
 	'When the language is Japanese, and line-break:strict, a browser will NOT allow a break before a PO character with East Asian Width of A, F, or W.',
 	],
 	'ja/css-text-line-break-ja-hyphens-normal.html': [
-	'CSS3 Text, linebreaks: hyphens (loose,ja)',
+	'CSS3 Text, linebreaks: hyphens (normal,ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] When the language is Japanese, and line-break:loose, a browser may allow a break before a hyphen.',
+	'[Exploratory] When the language is Japanese, and line-break:normal, a browser may allow a break before a hyphen.',
 	],
 	'ja/css-text-line-break-ja-iteration-normal.html': [
 	'CSS text, linebreaks: iteration (normal,ja)',
@@ -65,9 +65,9 @@ var tests = {
 	'When the language is Japanese, and line-break:normal, a browser will NOT allow a break before a PO character with East Asian Width of A, F, or W.',
 	],
 	'ja/css-text-line-break-ja-hyphens-loose.html': [
-	'CSS3 Text, linebreaks: hyphens (normal,ja)',
+	'CSS3 Text, linebreaks: hyphens (loose,ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] When the language is Japanese, and line-break:normal, a browser may allow a break before a hyphen.',
+	'[Exploratory] When the language is Japanese, and line-break:loose, a browser may allow a break before a hyphen.',
 	],
 	'ja/css-text-line-break-ja-iteration-loose.html': [
 	'CSS text, linebreaks: iteration (loose,ja)',
@@ -77,12 +77,12 @@ var tests = {
 	'ja/css-text-line-break-ja-in-loose.html': [
 	'CSS text, linebreaks: IN (loose,ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'When the language is Japanese, and line-break:loose, a browser may allow a break before an inseparable character.',
+	'[Exploratory] When the language is Japanese, and line-break:loose, a browser may allow a break before an inseparable character.',
 	],
 	'ja/css-text-line-break-ja-cpm-loose.html': [
 	'CSS text, linebreaks: centred punctuation (loose,ja)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'When the language is Japanese, and line-break:loose, a browser may allow a break before one of the centred punctuation characters listed.',
+	'[Exploratory] When the language is Japanese, and line-break:loose, a browser may allow a break before one of the centred punctuation characters listed.',
 	],
 	'ja/css-text-line-break-ja-po-loose.html': [
 	'CSS text, linebreaks: PO AFW (loose,ja)',
@@ -115,9 +115,9 @@ var tests = {
 	'When the language is Chinese, and line-break:strict, a browser will NOT allow a break before a PO character with East Asian Width of A, F, or W.',
 	],
 	'zh/css-text-line-break-zh-hyphens-normal.html': [
-	'CSS3 Text, linebreaks: hyphens (loose,zh)',
+	'CSS3 Text, linebreaks: hyphens (normal,zh)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] When the language is Chinese, and line-break:loose, a browser may allow a break before a hyphen.',
+	'[Exploratory] When the language is Chinese, and line-break:normal, a browser may allow a break before a hyphen.',
 	],
 	'zh/css-text-line-break-zh-iteration-normal.html': [
 	'CSS text, linebreaks: iteration (normal,zh)',
@@ -140,9 +140,9 @@ var tests = {
 	'When the language is Chinese, and line-break:normal, a browser will NOT allow a break before a PO character with East Asian Width of A, F, or W.',
 	],
 	'zh/css-text-line-break-zh-hyphens-loose.html': [
-	'CSS3 Text, linebreaks: hyphens (normal,zh)',
+	'CSS3 Text, linebreaks: hyphens (loose,zh)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'[Exploratory] When the language is Chinese, and line-break:normal, a browser may allow a break before a hyphen.',
+	'[Exploratory] When the language is Chinese, and line-break:loose, a browser may allow a break before a hyphen.',
 	],
 	'zh/css-text-line-break-zh-iteration-loose.html': [
 	'CSS text, linebreaks: iteration (loose,zh)',
@@ -152,12 +152,12 @@ var tests = {
 	'zh/css-text-line-break-zh-in-loose.html': [
 	'CSS text, linebreaks: IN (loose,zh)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'When the language is Chinese, and line-break:loose, a browser may allow a break before an inseparable character.',
+	'[Exploratory] When the language is Chinese, and line-break:loose, a browser may allow a break before an inseparable character.',
 	],
 	'zh/css-text-line-break-zh-cpm-loose.html': [
 	'CSS text, linebreaks: centred punctuation (loose,zh)',
 	'https://drafts.csswg.org/css-text-3/#line-breaking',
-	'When the language is Chinese, and line-break:loose, a browser may allow a break before one of the centred punctuation characters listed.',
+	'[Exploratory] When the language is Chinese, and line-break:loose, a browser may allow a break before one of the centred punctuation characters listed.',
 	],
 	'zh/css-text-line-break-zh-po-loose.html': [
 	'CSS text, linebreaks: PO AFW (loose,zh)',
