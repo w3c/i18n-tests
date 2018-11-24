e916c6db75647f8c4261e847675ad6cb85e610fe
r12a <ishida@w3.org>
2018-11-24 09:13:51 +0000

    _newresults.js

--- tools/_newresults.js
+++ tools/_newresults.js
@@ -1,25 +1,109 @@
 var newresults = {
 
 
-'text-orientation/text-orientation-upright-001.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:16:54' }
+'q-001.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:35:50' }
 	],
-'text-orientation/text-orientation-upright-002.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:16:57' }
+'q-002.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:35:57' }
 	],
-'text-combine-upright/text-combine-upright-all-001.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:16:59' }
+'q-003.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:35:58' }
 	],
-'text-combine-upright/text-combine-upright-all-002.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:17:0' }
+'q-004.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:16' }
 	],
-'text-combine-upright/text-combine-upright-digits-001.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:17:1' }
+'q-005.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:19' }
 	],
-'text-combine-upright/text-combine-upright-digits-002.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:17:3' }
+'q-lang-001.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:23' }
 	],
-'text-combine-upright/text-combine-upright-digits-004.html': [
-	{browser:'Safari', status:'fail', ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6', date:'4-3-2018  17:17:4' }
+'q-lang-002.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:30' }
+	],
+'q-lang-am.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:35' }
+	],
+'q-lang-ar.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:40' }
+	],
+'q-lang-bn.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:43' }
+	],
+'q-lang-chr.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:50' }
+	],
+'q-lang-el.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:36:58' }
+	],
+'q-lang-fa.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:1' }
+	],
+'q-lang-fr.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:6' }
+	],
+'q-lang-fr-ch.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:9' }
+	],
+'q-lang-gu.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:12' }
+	],
+'q-lang-he.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:19' }
+	],
+'q-lang-hi.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:25' }
+	],
+'q-lang-hu.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:29' }
+	],
+'q-lang-ja.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:32' }
+	],
+'q-lang-km.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:37' }
+	],
+'q-lang-ko.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:40' }
+	],
+'q-lang-lo.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:46' }
+	],
+'q-lang-my.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:49' }
+	],
+'q-lang-nl.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:54' }
+	],
+'q-lang-pa.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:37:57' }
+	],
+'q-lang-ta.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:1' }
+	],
+'q-lang-th.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:5' }
+	],
+'q-lang-zh-hans.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:11' }
+	],
+'q-lang-zh-hant.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:14' }
+	],
+'q-lang-zh.html': [
+	{browser:'SafariMobile', status:'pass', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:17' }
+	],
+'q-multilingual-nesting-001.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:20' }
+	],
+'q-multilingual-nesting-002.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:22' }
+	],
+'q-multilingual-nesting-003.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:25' }
+	],
+'q-multilingual-nesting-004.html': [
+	{browser:'SafariMobile', status:'fail', ua:'Mozilla/5.0 (iPad; CPU OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C153 Safari/604.1', date:'3-5-2018  13:38:27' }
 	]
 }
