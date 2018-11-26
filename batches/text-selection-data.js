562d70bde1c098f7783a5b13b059ec84900a16f1
r12a <ishida@w3.org>
2018-11-26 17:33:26 +0000

    + text-selection

---
+++ batches/text-selection-data.js
@@ -0,0 +1,16 @@
+var testresults = {
+
+
+"double-click-ar-001.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0", "date":"26-11-2018  17:27:23" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36", "date":"26-11-2018  17:28:21" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.15", "date":"26-11-2018  17:29:36" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134", "date":"26-11-2018  18:31:37" }
+	],
+"double-click-ar-002.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0", "date":"26-11-2018  17:27:24" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36", "date":"26-11-2018  17:28:23" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.15", "date":"26-11-2018  17:29:47" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134", "date":"26-11-2018  18:31:43" }
+	]
+}
