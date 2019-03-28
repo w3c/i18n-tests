cf9221d85fe04ab6784b1fe66c7e079c84cfba84
r12a <ishida@w3.org>
2019-03-28 07:26:41 +0000

    + case conversion results & tweaks

---
+++ batches/case-conversion-data.js
@@ -0,0 +1,70 @@
+var testresults = {
+
+
+"greek/text-transform-lc-el-consonants.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:29" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:45" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:22" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:39" }
+	],
+"greek/text-transform-lc-el-final-sigma.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:31" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:46" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:23" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:41" }
+	],
+"greek/text-transform-lc-el-monotonic-vowels.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:37" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:46" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:25" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:44" }
+	],
+"greek/text-transform-uc-el-consonants.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:39" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:47" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:26" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:46" }
+	],
+"greek/text-transform-uc-el-final-sigma.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:40" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:48" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:27" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:48" }
+	],
+"greek/text-transform-uc-el-monotonic-vowels.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:42" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:49" }, 
+	{"browser":"Safari", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:29" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:51" }
+	],
+"greek/text-transform-uc-el-monotonic-vowels-exp.html": [
+	{"browser":"Firefox", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:27:59" }, 
+	{"browser":"Chrome", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:51" }, 
+	{"browser":"Safari", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:41" }, 
+	{"browser":"Edge", "status":"fail", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:54" }
+	],
+"greek/text-transform-uc-el-monotonic-vowels-diphthongs.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:28:7" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:53" }, 
+	{"browser":"Safari", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:43" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:56" }
+	],
+"greek/text-transform-uc-el-monotonic-vowels-disjunctive-eta.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:28:10" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:55" }, 
+	{"browser":"Safari", "status":"fail", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:45" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:4:57" }
+	],
+"greek/text-transform-cap-el-consonants.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:28:14" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:56" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:49" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:5:2" }
+	],
+"greek/text-transform-cap-el-monotonic-vowels.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0", "date":"28-3-2019  6:28:17" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "date":"28-3-2019  6:56:58" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15", "date":"28-3-2019  7:0:52" }, 
+	{"browser":"Edge", "status":"pass", "ua":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "date":"28-3-2019  8:5:14" }
+	]
+}
