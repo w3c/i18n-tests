fbf943f0ac869706e7d4e4fcb7f7f1b14498ca0b
r12a <ishida@w3.org>
2022-03-14 17:48:10 +0000

    text-shadow batch files

---
+++ batches/text-shadow-data.js
@@ -0,0 +1,19 @@
+var testresults = {
+
+
+"text-shadow-cursive-arb.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0", "date":"14-3-2022  17:36:58", "notes":"" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36", "date":"14-3-2022  17:38:21", "notes":"" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15", "date":"14-3-2022  17:39:49", "notes":"" }
+	],
+"text-shadow-cursive-nqo.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0", "date":"14-3-2022  17:36:59", "notes":"" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36", "date":"14-3-2022  17:38:21", "notes":"" }, 
+	{"browser":"Safari", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15", "date":"14-3-2022  17:39:50", "notes":"" }
+	],
+"text-shadow-cursive-mn.html": [
+	{"browser":"Firefox", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0", "date":"14-3-2022  17:36:59", "notes":"" }, 
+	{"browser":"Chrome", "status":"pass", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36", "date":"14-3-2022  17:38:22", "notes":"" }, 
+	{"browser":"Safari", "status":"skipped", "ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15", "date":"14-3-2022  17:40:0", "notes":"" }
+	]
+}
