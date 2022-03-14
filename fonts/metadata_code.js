4e607bbdf3b24e9e1cf085631671cb380ffa3a6b
r12a <ishida@w3.org>
2022-03-14 17:51:05 +0000

    metadata code & style rewrite

--- fonts/metadata_code.js
+++ fonts/metadata_code.js
@@ -2,19 +2,36 @@ function showAssertion () {
     document.getElementById('assertion').innerHTML='Assertion: '+document.querySelector('meta[name=assert]').content
     }
 
-function showHelpLinks () {
-    out = 'Links: '
+function showLinks (path) {
+    var out = ''
+    
+    // do spec links
     var links = document.querySelectorAll('link[rel=help]')
     for (var i=0;i<links.length;i++) {
-        if (i>0) out += ', '
-        out += `<a target="_blank" href="${ links[i].href }">${ links[0].href }</a>`
-        document.getElementById('helpLinks').innerHTML = out
+        if (i>0) out += ' • '
+        out += `<a target="_blank" href="${ links[i].href }">Spec link</a>`
         }
+    
+    // do results links
+    var links = document.querySelectorAll('link[rel=prev]')
+    if (out != '' && links.length > 0) out += ' • '
+    for (var i=0;i<links.length;i++) {
+        if (i>0) out += ' • '
+        out += `<a target="_blank" href="${ links[i].href }">Results link</a>`
+        }
+    
+    // do test home link
+    var links = document.querySelectorAll('link[rel=top]')
+    if (out != '' && links.length > 0) out += ' • '
+    if (links.length > 0) out += `<a target="_blank" href="${ links[0].href }">Test home</a>`
+
+    document.getElementById('links').innerHTML = `Links: ${ out }`
     }
 
 
 
-function addToInfo () {
+function addToInfo (path) {
     showAssertion()
-    showHelpLinks()
+    //showHelpLinks() //showResultsPage(document.getElementById('resultsPage'))
+    showLinks(path)
     }
